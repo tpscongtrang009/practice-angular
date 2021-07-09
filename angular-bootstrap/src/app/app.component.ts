21f9f1bc8c8938b043713c16807e1ae5fc67d657
kieuanh99 <kieuanhtran2909@gmail.com>
2021-07-09 09:58:29 +0700

    commit bai 1

---
+++ angular-bootstrap/src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  public showAdd = true;
+  public showUpdate = false;
+  public users: any[];
+  public userAdd: any;
+  public userEdit: any;
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.userAdd = {};
+    this.loadData();
+  }
+
+  loadData() {
+    this.userService.getList().subscribe(
+      (response: any) => {
+        this.users = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteUser(id: number) {
+    let confirmDelete = confirm('Are you sure to delete this user ?');
+    if (confirmDelete) {
+      this.userService.deleteUserById(id).subscribe((response: any) => {
+        if (response) {
+          alert('Deleted');
+          this.loadData();
+          this.showAdd = true;
+          this.showUpdate = false;
+        }
+      });
+    }
+  }
+
+  addForm() {
+    this.userService.addUser(this.userAdd).subscribe((data: any) => {
+      if (data) {
+        alert('User was successfully added');
+        this.loadData();
+        this.userAdd = {};
+      }
+    });
+  }
+
+  editUser(id: number) {
+    this.userService.getUserById(id).subscribe((data: any) => {
+      this.userEdit = data;
+      this.showAdd = false;
+      this.showUpdate = true;
+    });
+  }
+
+  updateForm() {
+    this.userService.updateUser(this.userEdit).subscribe((response: any) => {
+      if (response) {
+        alert('Saved');
+        this.userEdit = {};
+
+        this.loadData();
+        this.showAdd = true;
+        this.showUpdate = false;
+      }
+    });
+  }
+}
---
+++ angular-bootstrap/src/app/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userApi = 'http://localhost:8080/';
+  constructor(private http: HttpClient) {}
+
+  getList():Observable<any[]>{
+    return this.http.get<any[]>(this.userApi+'users/')
+  }
+  getUserById(id:number):Observable<any[]>{
+    return this.http.get<any[]>(this.userApi+'userById/'+id)
+  }
+  deleteUserById(id:number):Observable<any[]>{
+    return this.http.delete(this.userApi+'deleteUser/'+id , {responseType: 'text'}).pipe(map((response:any) => response));
+  }
+
+  addUser(data:any):Observable<any[]>{
+    return this.http.post<any[]>(this.userApi+'addUser/', data)
+  }
+
+  updateUser(data:any):Observable<any[]>{
+    return this.http.put<any[]>(this.userApi+'updateUser/', data)
+  }
+}
