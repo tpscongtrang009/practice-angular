ce4d91d4e0869f6ec072d098a60461489b3fdb9f
kieuanh99 <kieuanhtran2909@gmail.com>
2021-07-09 10:10:21 +0700

    commit bai 2

---
+++ angular-material/src/app/app.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'phone','email','actions'];
+  public showAdd = true;
+  public showUpdate = false;
+  public users: any[];
+  public userAdd: any;
+  public userEdit: any;
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.userAdd = {};
+    this.loadData();
+
+  }
+
+  loadData() {
+    this.userService.getList().subscribe(
+      (response: any) => {
+        this.users = response;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteUser(id: number) {
+    let confirmDelete = confirm('Are you sure to delete this user ?');
+    if (confirmDelete) {
+      this.userService.deleteUserById(id).subscribe((response: any) => {
+        if (response) {
+          alert('Deleted');
+          this.loadData();
+        }
+      });
+    }
+  }
+
+  addForm() {
+    this.userService.addUser(this.userAdd).subscribe((data: any) => {
+      if (data) {
+        alert('Add New User');
+        this.loadData();
+        this.userAdd = {};
+      }
+    });
+  }
+
+  editUser(id: number) {
+    this.userService.getUserById(id).subscribe((data: any) => {
+      this.userEdit = data;
+      this.showAdd = false;
+      this.showUpdate = true;
+    });
+  }
+
+  updateForm() {
+    this.userService.updateUser(this.userEdit).subscribe((response: any) => {
+      if (response) {
+        alert('Saved');
+        this.userEdit = {};
+
+        this.loadData();
+        this.showAdd = true;
+        this.showUpdate = false;
+      }
+    });
+  }
+}
---
+++ angular-material/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
+import { UserService } from './user.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatRippleModule } from '@angular/material/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+@NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRippleModule
+
+  ],
+  
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
